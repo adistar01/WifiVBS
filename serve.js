8dfabdaef63b1781a25e3b9f6a6b1332da05d599
adistar01 <aditya21072000@gmail.com>
2022-07-19 11:51:28 +0530

    Second Commit

--- serve.js
+++ serve.js
@@ -4,24 +4,27 @@ const app = express()
 const port = 3000
 const wifi = require('wifi.js');
 
+// Initialize wifi module
+// Absolutely necessary even to set interface to null
+wifi.init({
+  iface: 'wifi' // network interface, choose a random wifi interface if set to null
+  });
+
+var eval;
+
+ wifi.getCurrentConnections((error, currentConnections) => {
+ if (error) {
+   eval=error;
+ } else {
+   eval=currentConnections;
+   
+ }
+ });
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  // Initialize wifi module
-// Absolutely necessary even to set interface to null
-   wifi.init({
-   iface: 'wifi' // network interface, choose a random wifi interface if set to null
-   });
-
-  wifi.getCurrentConnections((error, currentConnections) => {
-  if (error) {
-    res.send(error);
-  } else {
-    res.send(currentConnections);
-    
-  }
-  });
+  res.send(eval);
+  
 })
 
 app.post('/', (request, response) => {
